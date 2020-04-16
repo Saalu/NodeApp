2fa787b5c47398ce2799509a9cf07aabfc6eeafd
unknown <saaluissaka>
2020-04-16 07:09:13 +0000

    Initial Setup

---
+++ server.js
@@ -0,0 +1,26 @@
+// if(process.env.NODE_ENV !== 'production'){
+//     require('dotenv').parse()
+// }
+
+const express = require('express')
+const app = express()
+const expressLayouts= require('express-ejs-layouts')
+const mongoose = require('mongoose')
+
+
+const Router = require('./routes/index')
+ const key = require('./config/key').DATABASE_URL
+
+app.set('view engine', 'ejs')
+app.set('views', __dirname + '/views')
+app.set('layout', 'layouts/layout')
+app.use(expressLayouts)
+app.use(express.static('public'))
+
+
+mongoose.connect(key, {useNewUrlParser:true, useUnifiedTopology:true})
+.then(()=>console.log('MongoDB connected'))
+.catch(err=>console.log('Error: ', err))
+app.use('/', Router)
+
+app.listen(process.env.PORT || 5000, console.log('Server started!!!'))
